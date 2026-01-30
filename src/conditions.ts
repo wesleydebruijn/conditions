87dfb7668ed48d67f7dce0d5d1ee2d8f535637d4
Wesley de Bruijn <wesley.dbr@live.nl>
2026-01-30 11:20:36 +0100

    remove toggle

--- src/conditions.ts
+++ src/conditions.ts
@@ -52,8 +52,6 @@ export default class Conditions {
 
   private input: HTMLInputElement | HTMLTextAreaElement;
   private wrapperElement: HTMLElement;
-  private toggleElement: HTMLButtonElement;
-  private codeVisible: boolean = false;
 
   constructor(
     input: HTMLInputElement | HTMLTextAreaElement | string,
@@ -67,7 +65,6 @@ export default class Conditions {
       this.groups = deserialize(this.input.value);
       this.wrapperElement.remove();
       this.wrapperElement = create('div', 'conditions-wrapper');
-      visible(this.wrapperElement, !this.codeVisible);
 
       this.render();
     });
@@ -76,24 +73,12 @@ export default class Conditions {
     this.wrapperElement = create('div', 'conditions-wrapper');
     visible(this.input, false);
 
-    this.toggleElement = create('button', 'conditions-btn conditions-btn-outline');
-    this.toggleElement.textContent = 'Code View';
-    this.toggleElement.addEventListener('click', () => {
-      this.codeVisible = !this.codeVisible;
-      this.toggleElement.textContent = this.codeVisible ? 'Builder View' : 'Code View';
-
-      visible(this.input, this.codeVisible);
-      visible(this.wrapperElement, !this.codeVisible);
-    });
-    this.input.before(this.toggleElement);
-
     this.render();
   }
 
   public destroy() {
     this.input.conditions = null;
     this.wrapperElement.remove();
-    this.toggleElement.remove();
 
     visible(this.input, true);
   }
